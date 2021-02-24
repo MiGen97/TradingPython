39d122420d7d252694124f3507c6d6a63e192ef9
Mihail P <mihailpascu21@gmail.com>
2021-02-24 09:12:00 +0200

    RiskOfRuin v0.3

--- MoneyManagement_NauzerBalsara/RiskOfRuin.py
+++ MoneyManagement_NauzerBalsara/RiskOfRuin.py
@@ -4,42 +4,64 @@
 #   Purpose:        Functions for calculating the Risk of Ruin                                  #
 #   Documentation:  Nauzer J. Balsara - Money Management Strategies for Future Traders          #
 #                   Chapter 2 The Dynamics of Ruin                                              #
-#   Revision:       v0.1                                                                        #
+#   Version:        0.1 --- initial version                                                     #
+#   Version:        0.2 --- improved the calculation of riscOfRuin                              #
+#   Version:        0.3 --- added the calculation for riscOfRuin when payoffRatio > 2           #
 #   Inputs:         a.successProbability - the probability for a trade to be successfully       #
 #                   b.payoffRatio - the ration between the possible profit with possible loss   # 
 #                   c.unitsOfCapital - the number of units of capital available for trading     #
+#                                      (total capital divided by the capital that is risked     #
+#                                       in a trade)                                             #
 #   Output:         riskOfRuin - value between 0 and 1 that sure the probability of ruining     #
 #                                the account ( the probability to BLOW the account )            #
 #################################################################################################
+import random
 
 def calculateRiskOfRuin(successProbability,payoffRatio,unitsOfCapital):
-    #asume firsly the riskOfRuin to be 0 (impossible to ruin on this trade)
-    riskOfRuin = 0.0
+    #asume firsly the riskOfRuin to be 1 (certain)
+    riskOfRuin = 1.0
+    #translate parameters in mathematical notations
     p = successProbability
     q = 1.0 - successProbability
     k = unitsOfCapital
 
-    #call the three different functions 
+    #call the three different functions for riskOfRuin calculation
     if(payoffRatio == 1):
-        if(q >= p ):
-            riskOfRuin = 1.0
-        else:
-            riskOfRuin = __calculateRiskOfRuinWithPayoffRationOne(p,q,k)
+        if( q < p ):
+            riskOfRuin = __calculateRiskOfRuinPayoffRationOne(p,q,k)
     elif(payoffRatio == 2):
-        if(q >= 2*p ):
-            riskOfRuin = 1.0
-        else:
-            riskOfRuin = __calculateRiskOfRuinWithPayoffRationTwo(p,q,k)
+        if( q < 2*p ):
+            riskOfRuin = __calculateRiskOfRuinPayoffRationTwo(p,q,k)
     else:
-        riskOfRuin = __calculateRiskOfRuinWithPayoffRation(p,q,k)
-    return riskOfRuin
+        riskOfRuin = __calculateRiskOfRuinPayoffRationGreaterThanTwo(p,q,k,payoffRatio)
     
-def __calculateRiskOfRuinWithPayoffRationOne(p,q,k):
+    #avoid to send a wrong riskOfRuin
+    if (riskOfRuin < 0 or riskOfRuin > 1):
+        return -1
+    else:
+        return riskOfRuin
+    
+def __calculateRiskOfRuinPayoffRationOne(p,q,k):
     return ( q/p ) **k
 
-def __calculateRiskOfRuinWithPayoffRationTwo(p,q,k):
+def __calculateRiskOfRuinPayoffRationTwo(p,q,k):
     return ((0.25 + q/p )**0.5 - 0.5) **k
 
-#look-up tables found at pages 13-14
-def __calculateRiskOfRuinWithPayoffRation(p,q,k):
-    #return look-up-table
+#simulation found at pages 17-20
+def __calculateRiskOfRuinPayoffRationGreaterThanTwo(p,q,k,payoffRatio):
+    totalCapital=k
+    nrOfRuins = 0
+    nrOfWins = 0
+    #run simulation
+    for i in range(100000):
+        while ((totalCapital < (100*k)) or (totalCapital <= 0) )
+            fraction = random.random()  #!!!ATTENTION: this returns only numbers in interval [0.0,1.0)
+            if ( fraction <= q ):
+                totalCapital = totalCapital - 1
+            else:
+                totalCapital = totalCapital + (1 * payoffRatio)
+        if totalCapital <= 0 :
+            nrOfRuins += 1
+        if totalCapital >= (100*k):
+            nrOfWins +=1
+    return nrOfRuins/100000
