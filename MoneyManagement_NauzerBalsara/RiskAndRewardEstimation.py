7ba27b3c65aa645842867d3134608c16e8da28f9
Mihail P <mihailpascu21@gmail.com>
2021-02-25 23:18:25 +0200

    Added templates for chapter3 scripts

---
+++ MoneyManagement_NauzerBalsara/RiskAndRewardEstimation.py
@@ -0,0 +1,44 @@
+#################################################################################################
+#   Author:         Pascu Mihail-Eugen                                                          #
+#   Date&Hour:      25.02.2021 / 10:11PM                                                        #
+#   Purpose:        Functions for Detecting pattern to estimate the risk and reward             #
+#   Documentation:  Nauzer J. Balsara - Money Management Strategies for Future Traders          #
+#                   Chapter 3 Estimating Risk and Reward                                        #
+#   Version:        0.1 --- initial version                                                     #
+#   Inputs:         a.                                                                          #
+#   Output:                                                                                     #
+#################################################################################################
+
+def estimateRiskAndReward():
+    #select between Head-and-Shoulders, Double/triple tops and bottoms, Saoucers or rounded tops and bottoms and V-formations/spikes/island tops and bottoms
+    #or symmetrical and right-angle triangles, wedges, flags
+    
+def detectHeadAndShoulders():
+    #use machine learning to detect the pattern 
+    #help: theory pages 25-30
+    #help: https://github.com/anandanand84/technicalindicators/tree/master/src
+    #help: https://github.com/antoine-mtl/Head-and-shoulders-pattern-detection/blob/master/Train_Test.ipynb
+    
+def detectDoubleTopsAndBottoms():
+    #can be made to detect without ML (use machine learning to detect the pattern)
+    #help: theory pages 30-34
+    
+def detectSacersAndRoundedTopsAndBottoms():
+    #help: theory pages 34-35
+    
+def detectV_FormationsAndSpikesAndIslandReversals():
+    #help: theory pages 35-41
+    
+def detectSymmetricalAndRight_AngleTriangles():
+    #help: theory pages 41-43
+
+def detectWedges():
+    #help: theory pages 43-44
+    
+def detectFlags():
+    #help: theory pages 44-46
+    
+def applyWavesAndRetracements():
+    #help: theory page 48
+    #when there is no pattern detected, or new high or lows are reached
+    #can be used Elliot Waves, Fibonacci, etc.
