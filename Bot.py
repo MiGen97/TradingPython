a37fb09cc90b436200b21cf23c8c9818de135968
Mihail P <mihailpascu21@gmail.com>
2021-03-30 22:17:06 +0300

    Finished the initialization phase for the bot

--- Bot.py
+++ Bot.py
@@ -1,4 +1,9 @@
+####################################################
+#Import section#
+####################################################
 import MetaTrader5 as mt5
+import matplotlib.pyplot as plt
+from MoneyManagement_NauzerBalsara import RiskOfRuin as ror
 import time
 from datetime import datetime
 # import pytz module for working with time zone
@@ -7,196 +12,224 @@ import pytz
 import pandas as pd
 pd.set_option('display.max_columns', 500) # number of columns to be displayed
 pd.set_option('display.width', 1500)      # max table width to display
+from statistics import stdev
+import numpy as np
 
-from MoneyManagement_NauzerBalsara import RiskOfRuin as ror
+####################################################
+#Global Variables section#
+####################################################
 
-# # display data on the MetaTrader 5 package
-# print("MetaTrader5 package author: ",mt5.__author__)
-# print("MetaTrader5 package version: ",mt5.__version__)
- 
-# # risk of ruin testing
-successProbability = 0.35
-payoffRatio = 2.1
-unitsOfCapital = 100
-roundsOfTesting=(int)(1000000/unitsOfCapital)
-riskOfRuin=ror.calculateRiskOfRuin(successProbability,payoffRatio,unitsOfCapital,roundsOfTesting)
-print("riskOfRuin="+str(riskOfRuin))
+#constants
+lotDimension=100000 #
 
-# establish MetaTrader 5 connection
-if not mt5.initialize():
-    print("initialize() failed, error code =",mt5.last_error())
-    quit()
- 
- 
- 
-#login data
-account=41215438
-passwd="Fo9D2iXoaKiP"
-tradingServer="AdmiralMarkets-Demo"
-authorized=mt5.login(login=account, password=passwd, server=tradingServer)
-if authorized:
-    # display trading account data 'as is'
-    print(mt5.account_info())
-    # display trading account data in the form of a list
-    print("Show account_info()._asdict():")
-    account_info_dict = mt5.account_info()._asdict()
-    for prop in account_info_dict:
-        print("  {}={}".format(prop, account_info_dict[prop]))
-else:
-    print("failed to connect at account #{}, error code: {}".format(account, mt5.last_error()))
- 
- 
- 
- 
- 
-# display data on MetaTrader 5 version
-print(mt5.version())
-# display info on the terminal settings and status
-terminal_info=mt5.terminal_info()
-if terminal_info!=None:
-    # display the terminal data 'as is'
-    print(terminal_info)
-    # display data in the form of a list
-    print("Show terminal_info()._asdict():")
-    terminal_info_dict = mt5.terminal_info()._asdict()
-    for prop in terminal_info_dict:
-        print("  {}={}".format(prop, terminal_info_dict[prop]))
+#variables set at initialization
+balance=0
+oldBalance=0
+leverage=0
+rates=[]
+last_time=0
+
+#variables that must be set after backtesting
+successProbability=0.33
+historicalReturns=[0.00, - 1.20, - 7.50, 0.00, - 1.20, - 10.80, 0.00, - 1.20,  92.40, 0.00, - 1.20, - 28.50, 0.00, - 1.20, - 15.90, 0.00, - 1.20, - 5.40, 0.00, - 1.20, - 5.10, 0.00, - 1.20,  1.50, 0.00, - 1.20, - 20.85, 0.00, - 1.20, - 21.15, 0.00, - 1.20, - 25.05, 0.00, - 1.20,  117.60, 0.00, - 1.20, - 7.50, 0.00, - 1.20,  57.60, 0.00, - 1.20, - 9.00, 0.00, - 1.20,  67.80, 0.00, - 1.20, - 26.10, 0.00, - 1.20, - 30.15, 0.00, - 1.20, - 23.40, 0.00, - 1.20,  76.50, 0.00, - 1.20,  70.80, 0.00, - 1.20, - 28.80, 0.00, - 1.20, - 34.95, 0.00, - 1.20,  93.90, ]
+
+#variables to be optimized
+payoffRatio=2.5
+lotSize=0.2
+
+
+
+
+
+####################################################
+#Generic Functions section#
+####################################################
+def InitializeMetaTrader5():
+    print("====================================================================================")
+    # establish MetaTrader 5 connection
+    if not mt5.initialize():
+        print("The initialization of MetaTrader5 failed, error code =",mt5.last_error())
+        quit()
+    #login data
+    account=41215438
+    passwd="Fo9D2iXoaKiP"
+    tradingServer="AdmiralMarkets-Demo"
+    authorized=mt5.login(login=account, password=passwd, server=tradingServer)
+    if authorized:
+        # display trading account data in the form of a list
+        
+        print("Show the account infos:")
+        account_info_dict = mt5.account_info()._asdict()
+        for prop in account_info_dict:
+            print("  {}={}".format(prop, account_info_dict[prop]))
+    else:
+        print("Failed to connect at account #{}, error code: {}".format(account, mt5.last_error()))
+        quit()
     print()
-   # convert the dictionary into DataFrame and print
-    df=pd.DataFrame(list(terminal_info_dict.items()),columns=['property','value'])
-    print("terminal_info() as dataframe:")
-    print(df)
+
+def DisplayTerminalInfos():
+    print("====================================================================================")
+    # display data on MetaTrader 5 version
+    print(mt5.version())
+    # display info on the terminal settings and status
+    terminal_info=mt5.terminal_info()
+    if terminal_info is not None:
+        terminal_info_dict = mt5.terminal_info()._asdict()
+        # # display data in the form of a list
+        # print("Show terminal_info()._asdict():")
+        # for prop in terminal_info_dict:
+            # print("  {}={}".format(prop, terminal_info_dict[prop]))
+        # print()
+       # convert the dictionary into DataFrame and print
+        df=pd.DataFrame(list(terminal_info_dict.items()),columns=['property','value'])
+        
+        print("The terminal infos are :")
+        print(df)
+    else:
+        print("Failed to get the terminal infos, error code: {}".format(mt5.last_error()))
+        quit()
+    print()
+
+def GetRates():
+    print("====================================================================================")
+    # get 10 EURUSD D1 bars from the current day
+    global rates 
+    rates = mt5.copy_rates_from_pos("EURUSD", mt5.TIMEFRAME_M1, 0, 10)
+    if rates is not None:
+        # create DataFrame out of the obtained data
+        rates_frame = pd.DataFrame(rates)
+        # convert time in seconds into the datetime format
+        rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
+         
+        # display data
+        print("\nDisplay dataframe with data")
+        print(rates_frame)
+        plt.plot(rates_frame['time'],rates['close'])
+        plt.ylabel('close price')
+        plt.xlabel('time')
+        plt.title('EURUSD last 10 close prices')
+        plt.show()
+    else:
+        print("Error on getting the rates, error code: {}".format(mt5.last_error()))
+        quit()
+    print() 
+
+def GetAccountBalance():
+    balance=mt5.account_info()._asdict()['balance']
+    if balance != 0:
+        print("The balance is {}.".format(balance))
+    else:
+        print("Error on getting the balance, error code: {}".format(mt5.last_error()))
+    return balance
+
+def GetAccountLeverage():
+    leverage=mt5.account_info()._asdict()['leverage']
+    if leverage != 0:
+        print("The leverage is {}.".format(leverage))
+    else:
+        print("Error on getting the leverage, error code: {}".format(mt5.last_error()))
+    return leverage
+
+def GetBuyPrice():
+    # display the last EURUSD tick
+    ask=mt5.symbol_info_tick("EURUSD")._asdict()['ask']
+    if ask != 0:
+        print("The ask is {}.".format(leverage))
+    else:
+        print("Error on getting the ask, error code: {}".format(mt5.last_error()))
+    return ask
+    return 
+
+
+
+
+
+####################################################
+#Bot Functions section#
+####################################################    
+def InitializeTheMoneyBot():
+    #initialize the comunication with MetaTrader5
+    InitializeMetaTrader5()
+    DisplayTerminalInfos()
     
-
-
-
-
-# attempt to enable the display of the EURUSD symbol in MarketWatch
-selected=mt5.symbol_select("EURUSD",True)
-if not selected:
-    print("Failed to select EURUSD")
-else:
-    # display EURUSD symbol properties
-    symbol_info=mt5.symbol_info("EURUSD")
-    if symbol_info!=None:
-        # display the terminal data 'as is'    
-        print(symbol_info)
-        print("EURUSD: spread =",symbol_info.spread,"  digits =",symbol_info.digits)
-        # display symbol properties as a list
-        print("Show symbol_info(\"EURUSD\")._asdict():")
-        symbol_info_dict = mt5.symbol_info("EURUSD")._asdict()
-        for prop in symbol_info_dict:
-            print("  {}={}".format(prop, symbol_info_dict[prop]))
+    #get the rates(Bars) from MetaTrader5
+    rates=GetRates()
     
-
-
-
-
-# subscribe to market depth updates for EURUSD (Depth of Market)
-if mt5.market_book_add('EURUSD'):
-  # get the market depth data 1 times in a loop
-   for i in range(1):
-        # get the market depth content (Depth of Market)
-        items = mt5.market_book_get('EURUSD')
-        # display the entire market depth 'as is' in a single string
-        print(items)
-        # now display each order separately for more clarity
-        if items:
-            for it in items:
-                # order content
-                print(it._asdict())
-        # pause for 5 seconds before the next request of the market depth data
-        time.sleep(5)
-  # cancel the subscription to the market depth updates (Depth of Market)
-   mt5.market_book_release('EURUSD')
-else:
-    print("mt5.market_book_add('EURUSD') failed, error code =",mt5.last_error())
+    #initialize other data
+    #1. balance
+    global balance
+    global oldBalance
+    balance=GetAccountBalance()
+    oldBalance=balance
+    #2. leverage
+    global leverage
+    leverage=GetAccountLeverage()
     
+def CalculateMoneyManagementIndicators():
+    #Ask if I want to calculate:
+    #1. RiskOfRuin
+    answer=input("Calculate RiskOfRuin?(Y/N)")
+    if answer in ['Y','y']:
+        roundsOfTesting=100000
+        buyPrice=GetBuyPrice();
+        unitsOfCapital=(balance*leverage)/(lotSize*lotDimension*buyPrice)
+        print("The units of capital are {}.".format(unitsOfCapital))
+        riskOfRuin=ror.calculateRiskOfRuin(successProbability,payoffRatio,unitsOfCapital,roundsOfTesting)
+        print("The RiskOfRuin with the current strategy is {}.".format(riskOfRuin))
     
+    # #2. Volatility of historical returns
+    answer=input("Calculate Volatility of historical returns?(Y/N)")
+    if answer in ['Y','y']:
+        volatilityOfReturns=stdev(historicalReturns)
+        print("The Volatility of historical returns with the current strategy is {}.".format(volatilityOfReturns))
+        
     
+    #3. Profitability index
+    answer=input("Profitability index?(Y/N)")
+    if answer in ['Y','y']:
+        profitabilityIndex=successProbability/(1-successProbability)*payoffRatio
+        if profitabilityIndex >= 2 and profitabilityIndex < 3 :
+            print("The Profitability index with the current strategy is {} and that is GOOD.".format(profitabilityIndex))
+        elif profitabilityIndex >= 3:
+            print("The Profitability index with the current strategy is {} and that is EXCELENT.".format(profitabilityIndex))
+        else :
+            print("The Profitability index with the current strategy is {} and that is BAD.".format(profitabilityIndex))
+
+def isNewBar():
+    global last_time
+    global rates
     
-    
-# get 10 EURUSD D1 bars from the current day
-rates = mt5.copy_rates_from_pos("EURUSD", mt5.TIMEFRAME_D1, 0, 10)
+    lastbar=mt5.copy_rates_from_pos("EURUSD", mt5.TIMEFRAME_M1, 0, 1)
+    print(lastbar['time'])
+    if last_time == 0:
+        last_time=lastbar['time']
+        return False
+    if last_time<lastbar['time'] :
+        last_time=lastbar['time']
+        rates=np.append(rates,lastbar)
+        return True
+    return False
+####################################################
+#Main section#
+####################################################
 
-# display each element of obtained data in a new line
-print("Display obtained data 'as is'")
-for rate in rates:
-    print(rate)
- 
-# create DataFrame out of the obtained data
-rates_frame = pd.DataFrame(rates)
-# convert time in seconds into the datetime format
-rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
- 
-# display data
-print("\nDisplay dataframe with data")
-print(rates_frame) 
+################
+#Initialization#
+################
+InitializeTheMoneyBot()
+CalculateMoneyManagementIndicators()
+#CalculateCurrentIndicators()
 
-
-
-# set time zone to UTC
-timezone = pytz.timezone("Etc/UTC")
-# create 'datetime' object in UTC time zone to avoid the implementation of a local time zone offset
-utc_from = datetime(2021, 3, 19, tzinfo=timezone)
-# request 100 EURUSD ticks starting from 10.01.2021 in UTC time zone
-ticks = mt5.copy_ticks_from("EURUSD", utc_from, 8050, mt5.COPY_TICKS_ALL)
-print("Ticks received:",len(ticks))
-
-# display data on each tick on a new line
-print("Display obtained ticks 'as is'")
-count = 0
-for tick in ticks:
-    count+=1
-    print(tick)
-    if count >= 10:
-        break
- 
-# create DataFrame out of the obtained data
-ticks_frame = pd.DataFrame(ticks)
-# convert time in seconds into the datetime format
-ticks_frame['time']=pd.to_datetime(ticks_frame['time'], unit='s')
- 
-# display data
-print("\nDisplay dataframe with ticks")
-print(ticks_frame.head(10))  
-
-
-
-
-# check the presence of active orders
-orders=mt5.orders_total()
-if orders>0:
-    print("Total orders=",orders)
-else:
-    print("Orders not found")
-    
-
-
-
-# display data on active orders on EURUSD
-orders=mt5.orders_get(symbol="EURUSD")
-if orders is None:
-    print("No orders on EURUSD, error code={}".format(mt5.last_error()))
-else:
-    print("Total orders on EURUSD:",len(orders))
-    # display all active orders
-    for order in orders:
-        print(order)
-print()
- 
-# get the list of orders on symbols whose names contain "*EUR*"
-eur_orders=mt5.orders_get(group="*EUR*")
-if (eur_orders is None) or (len(eur_orders) == 0):
-    print("No orders with group=\"*EUR*\", error code={}".format(mt5.last_error()))
-else:
-    print("orders_get(group=\"*EUR*\")={}".format(len(eur_orders)))
-    # display these orders as a table using pandas.DataFrame
-    df=pd.DataFrame(list(eur_orders),columns=eur_orders[0]._asdict().keys())
-    df.drop(['time_done', 'time_done_msc', 'position_id', 'position_by_id', 'reason', 'volume_initial', 'price_stoplimit'], axis=1, inplace=True)
-    df['time_setup'] = pd.to_datetime(df['time_setup'], unit='s')
-    print(df)
-
-# shut down connection to the MetaTrader 5 terminal
+######
+#Loop#
+######
+while 1:
+    if isNewBar() :
+        print("New bar")
+        #Implement the strategy
+    else:
+        print("Old bar")
+        time.sleep(30)
 mt5.shutdown()
 quit()
+ 
